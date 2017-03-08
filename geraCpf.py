cf48eb53ee57df9400a5ca883c5180777f488ba0
Lucas Ferreira dos santos <lucas.santos61@fatec.sp.gov.br>
2017-03-08 16:58:46 -0300

    Add files via upload
    Primeira versão que ainda não coloca a mascara no cpf , mas já gera CPF válido

---
+++ geraCpf.py
@@ -0,0 +1,33 @@
+
+def geraNumeros():
+	import random
+	numeros = []
+	x = 0
+	while x < 9:
+		numeros.append(random.randint(1,9))
+		x +=1 
+	return numeros
+
+def geraVerificador(cpf,aux):
+	x = 0
+	total = 0
+	while x < len(cpf):
+		total += (cpf[x] * (aux))
+		aux -= 1
+		x+=1
+	if(total%11 < 2 ):
+		cpf.append(0)
+	else:
+		cpf.append(11 - (total%11))
+	return cpf
+#def geraMascara(cpf):
+
+def geraCPF():
+	num = geraNumeros()
+	temp = geraVerificador(num,10)
+	temp = geraVerificador(temp,11)
+	cpf =  "".join(str(i) for i in temp)
+	return cpf	
+
+
+print(geraCPF())	
